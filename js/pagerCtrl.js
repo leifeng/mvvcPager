828f02bf6be742746b51446d117fbc2f3dfd01d6
leifeng <289528400@qq.com>
2015-08-27 10:51:08 +0800

    优化显示效果

--- js/pagerCtrl.js
+++ js/pagerCtrl.js
@@ -1,79 +1,81 @@
 var pagerCtrl = avalon.define('pagerCtrl', function (vm) {
+    vm.t = -1;
+    vm.n = 1;
+    vm.show = true;
     vm.totalPage = 1;//总页数
-    vm.totalCount = 0;//总记录数
-    vm.pageNo = 1;//当前夜
-    vm.pageSize = 10;//一页显示几条数据
-    vm.showCount = 0;//要显示几页
-    vm.firstPage = true;//是否首页
-    vm.lastPage = false;//是否末页
-    vm.list = [];//数组
-    vm.fn = null;//获取数据的方法
-    vm.index=0;//数组索引位置，为了加样式
-    //上一页
+    vm.totalCount = 0;//总记录
+    vm.pageNo = 1;//当前页
+    vm.pageSize = 10;//显示的记录数
+    vm.showPagers = 10;//显示的页数
+    vm.list = []; //页码
+    vm.fn = null;//获取数据方法
+    vm.index = 0;//当前页码索引
+    vm.lock=false;
     vm.toPre = function () {
-        if (!vm.firstPage) {
+        if (vm.pageNo >1) {
             vm.pageNo--;
             vm.index--;
             if(vm.index<0){
-                vm.index=0;
-                vm.list.unshift({index:vm.list[0].index-1,style:''});
-                vm.list.pop();
+                vm.n--;
             }
-            vm.fn(vm.pageNo, vm.pageSize);
-            vm.setClass();
+            if (vm.index <=0) {
+                vm.index = 0;
+            }
+            vm.getData();
         }
     };
-    //下一页
     vm.toNext = function () {
-        if (!vm.lastPage) {
+        if (vm.pageNo < vm.totalPage) {
             vm.pageNo++;
             vm.index++;
-            if(vm.index>vm.showCount-1){
-                vm.index=vm.showCount-1;
-                vm.list.push({index:vm.pageNo,style:''});
-                vm.list.shift();
+            if(vm.index>vm.showPagers-1){
+                vm.n++;
             }
-            vm.fn(vm.pageNo, vm.pageSize);
-            vm.setClass();
+            if (vm.index >= vm.showPagers) {
+                vm.index = vm.showPagers - 1;
+            }
+            vm.getData();
         }
+
     };
-    //点击某一页
-    vm.toNum = function (pageNo,index) {
-        vm.index=index;
-        vm.setClass();
-        vm.fn(pageNo, vm.pageSize);
+    vm.toNum = function (pageNo, index) {
+        vm.pageNo = pageNo;
+        vm.index = index;
+        vm.getData();
     };
-    //首页
     vm.toFirst = function () {
-        vm.index=0;
-        vm.pageNo=1;
-        vm.setList(vm.showCount);
-        vm.fn(1, vm.pageSize);
+        vm.pageNo = 1;
+        vm.index = 0;
+        vm.n=1;
+        vm.getData();
     };
-    //最后一页
     vm.toLast = function () {
-        var n=Math.min(vm.totalPage,vm.showCount);
-        vm.index=n-1;
-        vm.pageNo=vm.totalPage;
-        vm.setList(vm.showCount,vm.totalPage-n+1,vm.totalPage);
-        vm.fn(vm.totalPage, vm.pageSize);
+        vm.pageNo =vm.totalPage ;
+        vm.index = vm.showPagers - 1;
+        vm.n=vm.totalPage-vm.showPagers+1;
+        vm.getData();
     };
-    //加载分页数组
-    vm.setList=function(n,n1,n2){
-        vm.showCount = n;
-        vm.list=[];
-        var i=n1||vm.pageNo;
-        var x=n2||Math.min(vm.totalPage,n);
-        for(;i<=x;i++){
-            vm.list.push({index:i,style:''});
+    vm.setList = function (totalPage) {
+        if(!vm.lock){
+            vm.lock=true;
+            pagerCtrl.totalPage=totalPage;
+            vm.showPagers = Math.min(totalPage, vm.showPagers);
+            for (var i = 0; i < vm.showPagers; i++) {
+                vm.list.push(0);
+            }
         }
-        vm.setClass();
+
     };
-    //修改样式
-    vm.setClass=function(){
-        for(var i=0;i<Math.min(vm.totalPage,vm.showCount);i++){
-            vm.list[i]['style']='';
-        }
-        vm.list[vm.index]['style']='active';
+    vm.getData=function(pangNo){
+        clearTimeout(vm.t);
+        vm.t=setTimeout(function(){
+            var index=pangNo||vm.pageNo;
+            pagerCtrl.fn(index,vm.pageSize);
+        },500);
+    };
+    vm.init = function (num, pagesize, fn) {
+        vm.pageSize = pagesize;
+        vm.showPagers = num;
+        vm.fn = fn;
     }
 });
